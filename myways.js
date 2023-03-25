912b03dae309d1728119fba689e93123e1d92445
krishomni <128909654+krishomni@users.noreply.github.com>
2023-03-25 19:02:41 +0530

    Create myways.js

---
+++ myways.js
@@ -0,0 +1,36 @@
+const form= document.querySelector('form');
+const nameInput = document.querySelector('name');
+const emailInput= document.querySelector('email');
+const phoneInput= document.querySelector('phone');
+form.addEventListener('submit', (event)=> {
+event.preventDefault();
+const name=nameInput.value;
+const email=emailInput.value;
+const phone=phoneInput.value;
+fetch('https://example.com/api/get-user?email=${email}').then(response => {
+if(response.ok){
+return response.json();
+}
+else{
+throw new Error('Network response was not ok');
+}
+}).then(data => {
+alert('User found');
+}).catch(error=> {
+fetch('https://example.com/api/create-user',{
+method: 'POST',
+body: JSON.stringify({ name,email,phone}),
+headers:{
+'Content-Type": 'application/json'
+}
+}).then(response => {
+if(response.ok){
+alert('User created successfully');
+}else{
+throw new Error('Network response was not ok');
+}
+}).catch(error => {
+console.error('Error', error);
+});
+});
+});
